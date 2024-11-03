c48c305e944132b79c1a7955fde42380cfa3a542
Adrian Rogers <adritek@gmail.com>
2024-11-03 23:36:47 +0100

    Set player name and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import PlayerInfo from './Components/PlayerInfo';
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <PlayerInfo name="Player 1" symbol="X" />
+          <PlayerInfo name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
