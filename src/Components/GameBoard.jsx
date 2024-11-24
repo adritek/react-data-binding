04125dcda60380af592eb3ee0eed62b10b53983e
Adrian Rogers <adritek@gmail.com>
2024-11-24 23:23:03 +0100

    Lift derived gameboard to app, check for winning combinations

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,40 +1,15 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectedSquare(rowIndex, colIndex) {
-  //   setGameBoard((previousGameBoard) => {
-  //     const updatedGameBoard = [
-  //       ...previousGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
